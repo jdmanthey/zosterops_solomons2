c5316dd33adb7e38fde16d08c0446df2fbc7599a
Joe Manthey <jdmanthey@gmail.com>
2025-12-04 16:42:03 -0600

    Create 01_directory_setup.sh

---
+++ 01_directory_setup.sh
@@ -0,0 +1,18 @@
+# define main working directory
+workdir=/lustre/scratch/jmanthey/02_zosterops
+
+# make output directories
+cd ${workdir}
+
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_vcf
+mkdir 06_phylogenies
+mkdir 06_phylogenies/windows
+mkdir 07_dstats
+mkdir 07_dstats/windows
+mkdir 10_filter
+mkdir 11_filter_vcf
