16b041f124093d6db6592037403dc19a3dda9359
Joe Manthey <jdmanthey@gmail.com>
2025-12-23 14:51:26 -0600

    Update 08_cat_stats.sh

--- 08_cat_stats.sh
+++ 08_cat_stats.sh
@@ -1,8 +1,9 @@
-cd /lustre/scratch/jmanthey/02_zosterops/12_stats
+cd /lustre/scratch/jmanthey/02_zosterops/14_stats
 
 # combine the output for different windows into a single file 
 # first add a header for each file
-grep 'pop1' OZ246474.1__10000001__10100000__stats.txt > ../window_100kbp_stats.txt
+grep 'pop1' OZ246474.1__1000001__1025000__stats.txt > ../window_25kbp_stats.txt
 
 # add the stats to the file
-for i in $( ls *__stats.txt ); do grep -v 'pop1' $i >> ../window_100kbp_stats.txt; done
+for i in $( ls *__stats.txt ); do grep -v 'pop1' $i >> ../window_25kbp_stats.txt; done
+
