6f4a6cbdf0acb578c0bc59c3ab9008b39935aa31
Joe Manthey <jdmanthey@gmail.com>
2025-12-23 14:51:10 -0600

    Update 09_mcctree.sh

--- 09_mcctree.sh
+++ 09_mcctree.sh
@@ -1,3 +1,4 @@
+sumtrees.py --output=zosterops_25kbp.tre --min-clade-freq=0.01 zosterops_25kbp.trees
+
 sumtrees.py --output=zosterops_50kbp.tre --min-clade-freq=0.01 zosterops_50kbp.trees
 
-sumtrees.py --output=zosterops_100kbp.tre --min-clade-freq=0.01 zosterops_100kbp.trees
