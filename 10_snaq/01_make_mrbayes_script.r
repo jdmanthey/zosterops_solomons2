96925cd72ab8c0da767e3c0c1521f70b2f1d3b94
Joe Manthey <jdmanthey@gmail.com>
2026-01-16 16:41:00 -0600

    Create 01_make_mrbayes_script.r

---
+++ 10_snaq/01_make_mrbayes_script.r
@@ -0,0 +1,161 @@
+options(scipen=999)
+project_directory <- "/lustre/scratch/jmanthey/02_zosterops/06_phylogenies"
+directory_name <- "15_mrbayes"
+cluster <- "nocona"
+max_number_jobs <- 800
+
+# command to open environment you will need
+env_command <- "source activate bcftools" # if none, just put nothing in the quotes
+
+# vcf header location or file to extract the header (should be gzipped)
+vcf_header <- paste0(project_directory, "/OZ246513.1.recode.vcf.gz")
+
+# name of script to write
+output_script_name <- "01_mrbayes_array.sh"
+
+# read in reference index
+# filtered to only include genotyped chromosomes
+ref_index <- read.table("bZosLat1_subset.fai", stringsAsFactors=F)
+
+# define window size
+window_size <- 25000
+
+######################################
+######################################
+## only edit above this (unless you know specific edits you need below)
+######################################
+######################################
+
+# make directories
+dir.create(directory_name)
+
+# define intervals and write to helper files
+stat_helper1 <- list()
+stat_helper2 <- list()
+stat_helper3 <- list()
+counter <- 1
+for(a in 1:nrow(ref_index)) {
+  a_start <- 1
+  a_end <- a_start + window_size - 1
+  a_max <- ref_index[a,2]
+  a_windows <- ceiling((a_max - a_start) / window_size)
+  a_chromosome <- ref_index[a,1]
+  
+  # loop for defining helper info for each window
+  for(b in 1:a_windows) {
+    if(b == a_windows) {
+      a_end <- a_max
+    }
+    stat_helper1[[counter]] <- a_chromosome
+    stat_helper2[[counter]] <- a_start
+    stat_helper3[[counter]] <- a_end
+    
+    a_start <- a_start + window_size
+    a_end <- a_end + window_size
+    counter <- counter + 1
+  }
+}
+stat_helper1 <- unlist(stat_helper1)
+stat_helper2 <- unlist(stat_helper2)
+stat_helper3 <- unlist(stat_helper3)
+
+# calculate number of array jobs
+if(length(stat_helper3) > max_number_jobs) {
+  n_jobs_per_array <- ceiling(length(stat_helper3) / max_number_jobs)
+  n_array_jobs <- ceiling(length(stat_helper3) / n_jobs_per_array)
+} else {
+  n_array_jobs <- length(stat_helper3)
+  n_jobs_per_array <- 1
+}
+
+stat_helper1 <- c(stat_helper1, rep("x", n_jobs_per_array - length(stat_helper3) %% n_jobs_per_array))
+stat_helper2 <- c(stat_helper2, rep(1, n_jobs_per_array - length(stat_helper3) %% n_jobs_per_array))
+stat_helper3 <- c(stat_helper3, rep(1, n_jobs_per_array - length(stat_helper3) %% n_jobs_per_array))
+length(stat_helper3)
+stat_helper <- data.frame(chrom=as.character(stat_helper1), start=as.numeric(stat_helper2), end=as.numeric(stat_helper3))
+write.table(stat_helper, file=paste(directory_name, "/stat_helper.txt", sep=""), sep="\t", quote=F, row.names=F, col.names=F)
+
+# write the array script
+a.script <- paste(directory_name, "/", output_script_name, sep="")
+write("#!/bin/sh", file=a.script)
+write("#SBATCH --chdir=./", file=a.script, append=T)
+write(paste("#SBATCH --job-name=", "mrbayes", sep=""), file=a.script, append=T)
+write("#SBATCH --nodes=1 --ntasks=1", file=a.script, append=T)
+write(paste("#SBATCH --partition ", cluster, sep=""), file=a.script, append=T)
+write("#SBATCH --time=48:00:00", file=a.script, append=T)
+write("#SBATCH --mem-per-cpu=4G", file=a.script, append=T)
+write(paste("#SBATCH --array=1-", n_array_jobs, sep=""), file=a.script, append=T)
+write("", file=a.script, append=T)
+write(env_command, file=a.script, append=T)
+write("", file=a.script, append=T)
+
+write("# Set the number of runs that each SLURM task should do", file=a.script, append=T)
+write(paste("PER_TASK=", n_jobs_per_array, sep=""), file=a.script, append=T)
+write("", file=a.script, append=T)
+
+write("# Calculate the starting and ending values for this task based", file=a.script, append=T)
+write("# on the SLURM task and the number of runs per task.", file=a.script, append=T)
+write("START_NUM=$(( ($SLURM_ARRAY_TASK_ID - 1) * $PER_TASK + 1 ))", file=a.script, append=T)
+write("END_NUM=$(( $SLURM_ARRAY_TASK_ID * $PER_TASK ))", file=a.script, append=T)
+write("", file=a.script, append=T)
+
+write("# Print the task and run range", file=a.script, append=T)
+write("echo This is task $SLURM_ARRAY_TASK_ID, which will do runs $START_NUM to $END_NUM", file=a.script, append=T)
+write("", file=a.script, append=T)
+
+write("# Run the loop of runs for this task.", file=a.script, append=T)	
+write("for (( run=$START_NUM; run<=$END_NUM; run++ )); do", file=a.script, append=T)
+write("\techo This is SLURM task $SLURM_ARRAY_TASK_ID, run number $run", file=a.script, append=T)
+write("", file=a.script, append=T)
+write("\tsource activate bcftools", file=a.script, append=T)
+write("", file=a.script, append=T)
+
+
+write("\tchrom_array=$( head -n${run} stat_helper.txt | cut -f1 | tail -n1 )", file=a.script, append=T)
+write("", file=a.script, append=T)
+write("\tstart_array=$( head -n${run} stat_helper.txt | cut -f2 | tail -n1 )", file=a.script, append=T)
+write("", file=a.script, append=T)
+write("\tend_array=$( head -n${run} stat_helper.txt | cut -f3 | tail -n1 )", file=a.script, append=T)
+write("", file=a.script, append=T)
+
+# add header to output file
+header <- paste('\tgunzip -cd ', vcf_header, ' | grep "#" > ', project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep="")
+write(header, file=a.script, append=T)
+write("", file=a.script, append=T)
+
+#tabix command
+tabix_command <- paste("\ttabix ", project_directory, "/${chrom_array}.recode.vcf.gz ${chrom_array}:${start_array}-${end_array} >> ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep="")
+write(tabix_command, file=a.script, append=T)
+write("", file=a.script, append=T)
+
+# convert vcf to nexus
+rscript_command <- paste("\tRscript _write_mrbayes.r ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep="")
+write(rscript_command, file=a.script, append=T)
+write("", file=a.script, append=T)
+# remove unnecessary files at end
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.recode.vcf", sep=""), file=a.script, append=T)
+write("", file=a.script, append=T)
+write("\tconda deactivate", file=a.script, append=T)
+write("", file=a.script, append=T)
+
+# mrbayes command
+mrbayes_command <- paste0("\tmb ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex")
+write(mrbayes_command, file=a.script, append=T)
+write("", file=a.script, append=T)
+# remove unnecessary files at end
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.ckp", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.ckp~", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.mcmc", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.parts", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.trprobs", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.tstat", sep=""), file=a.script, append=T)
+write(paste("\trm ", project_directory, "/windows/${chrom_array}__${start_array}__${end_array}.nex.vstat", sep=""), file=a.script, append=T)
+
+
+
+
+write("", file=a.script, append=T)
+
+# finish
+write("done", file=a.script, append=T)
